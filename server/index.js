e54318468277137acdd4d87053f65340b58c9893
zimooo2 <zimooo2@gmail.com>
2020-04-30 12:43:29 -0700

    hopefully fix compiled folder and bottomup reference

--- server/index.js
+++ server/index.js
@@ -8,21 +8,25 @@ const app = express();
 app.use(bodyParser.json());
 app.use(pino);
 
-let totalCompiledFiles = 0; // return the number of files
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+var totalCompiledFiles = 0; // return the number of files
 fs.readdir( 'compiled', (error, files) => { 
-  totalCompiledFiles = files.length;
-  console.log('Total compiled files: ' + totalCompiledFiles);
+  global.totalCompiledFiles = files.length - 1;
+  console.log('Total compiled files: ' + global.totalCompiledFiles);
 });
 
 // POST method route
 app.post('/api/compile', function (req, res) {
   if (req.body && req.body.code) {
-    exec(`mkdir compiled/${totalCompiledFiles}`, (error, stdout, stderr) => {
+    exec(`mkdir compiled/${global.totalCompiledFiles}`, (error, stdout, stderr) => {
       //report errors
       if (error) { res.send(JSON.stringify({ greeting: `error: ${error.message}` })); return; }
       if (stderr) { res.send(JSON.stringify({ greeting: `stderr: ${stderr}` })); return; }
 
-      let currentFile = totalCompiledFiles++;
+      var currentFile = global.totalCompiledFiles;
+      global.totalCompiledFiles++;
 
       fs.writeFile(`compiled/${currentFile}/code.bgl`, req.body.code, function (err) {
         if (err) { res.send(JSON.stringify({ greeting: `error: ${err}` })); return; }
@@ -46,6 +50,12 @@ app.post('/api/compile', function (req, res) {
   }
 });
 
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
 
 app.listen(3001, () =>
   console.log('Express server is running on localhost:3001')
--- src/CodeEditor.js
+++ src/CodeEditor.js
@@ -7,28 +7,53 @@ import 'prismjs/components/prism-clike';
 import 'prismjs/components/prism-javascript';
 import 'prismjs/components/prism-haskell';
 import 'prismjs/themes/prism-dark.css'
- 
+import Button from '@material-ui/core/Button';
 
-const code = `function add(a, b) {
-  return a + b;
-}
+const code = `
+  type Board = Array(3,3) of Player & { Empty }
+  type Input = Position
+
+  initialBoard : Board
+  initialBoard ! (x, y) = Empty
 `;
 
 class CodeEditor extends React.Component{
-  state = { code };
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      code: code,
+      greeting: ''
+    };
+
+    // Simple POST request with a JSON body using fetch
+    fetch('/api/compile', {
+      method: 'POST',
+      headers:  {  
+                  'Accept': 'application/json, text/plain, */*',
+                  'Content-Type': 'application/json' 
+                },
+      body: JSON.stringify({ code: code })
+    })
+        .then(response => response.json())
+        .then(data => this.setState(data));
+  }
 
   render(){
     return (
-      <Editor
-        value={this.state.code}
-        onValueChange={code => this.setState({ code })}
-        highlight={code => highlight(code, languages.js)}
-        padding={10}
-        style={{
-          fontFamily: '"Fira code", "Fira Mono", monospace',
-          fontSize: 12,
-        }}
-      />
+      <div>
+        <Editor
+          value={this.state.code}
+          onValueChange={code => this.setState({ code })}
+          highlight={code => highlight(code, languages.js)}
+          padding={10}
+          style={{
+            fontFamily: '"Fira code", "Fira Mono", monospace',
+            fontSize: 12,
+          }}
+        />
+      </div>
     );
   }
 }
